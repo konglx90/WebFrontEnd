abcb7af99ab7a19aa6d381659c7e95b583ec7693
konglingxing <lingxingkong213712@sohu-inc.com>
2018-02-14 10:53:55 +0800

    add react-ts

---
+++ react-ts/src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div className="Button">
+        <p>button</p>
+      </div>
+    );
+  }
+}
+
+export default Button;
