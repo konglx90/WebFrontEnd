350915b8cd90352ff5822fff0f8867782df64911
konglingxing <lingxingkong213712@sohu-inc.com>
2018-03-11 19:02:22 +0800

    自定义react-script

--- react-azalea/react-scripts/template/src/App.js
+++ react-azalea/react-scripts/template/src/App.js
@@ -1,22 +1,8 @@
-// @flow
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.less';
-// import { agent } from './api';
+import './App.css';
 
-import SetStateTest from './react-feature-test/SetState';
-
-class App extends Component<{}> {
-
-  componentDidMount() {
-    // agent.News.getLastest();
-  }
-
-  hello = (n: number) => {
-    return n * 2;
-  }
-
+class App extends Component {
   render() {
     return (
       <div className="App">
@@ -24,7 +10,9 @@ class App extends Component<{}> {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <SetStateTest />
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
